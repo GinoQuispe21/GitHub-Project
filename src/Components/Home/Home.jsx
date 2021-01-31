2cb68f8282d6eb01fd26b5454726cba5c8ae0b0d
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-01-30 19:06:25 -0500

    Added Get User Info :zap:

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import URL from '../../baseURL';    
 
 const Home = () => {
+
+    const [username, setUsername] = React.useState();
+    const [info, setInfo] = React.useState();
+
+    const GetUserInfo = () =>{
+        axios.get(`${URL}users/${username}/followers`)
+        .then(res => {
+            setInfo(res.data);
+            console.log(info);
+        })
+    }
+
+    const onchange = (event) => {
+        setUsername(event.target.value);
+    }
+
     return (
         <div>
-            <h2>ola mundo</h2>
+            <input value={username} onChange={onchange}></input>
+            <h2>{username}</h2>
+            <h2>{info?.login}</h2>
+            <button onClick={GetUserInfo}>soy un bonito boton</button>
+            <div>
+                <p>{JSON.stringify(info)}</p>
+            </div>
         </div>
     )
 }
---
+++ src/baseURL.js
@@ -0,0 +1,1 @@
+export default URL = 'https://api.github.com/';
