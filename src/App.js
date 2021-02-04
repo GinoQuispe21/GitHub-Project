25e2851da08dc55bdb1039c635899780858aae56
Juan Carlos Hernández <jchernandez0312@gmail.com>
2021-02-04 18:03:20 -0500

    ✨✨React Router✨✨
    ✨✨Router React✨✨

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import Home from './Components/Home/Home';
+import UserInfo from './Components/UserInfo/UserInfo.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/users/:username">
+            <UserInfo />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
     </>
   );
 }
--- src/Components/UserInfo/UserInfo.jsx
+++ src/Components/UserInfo/UserInfo.jsx
@@ -1,18 +1,22 @@
 import React, {useEffect} from 'react'
+import {useParams} from "react-router-dom";
 
 const UserInfo = (info) => {
 
     //avatar_url
     const [avatarUrl, setAvatarUrl] = React.useState('https://avatars.githubusercontent.com/');
-    const [username, setUsername] = React.useState();
+    // const [username, setUsername] = React.useState();
+    let { username } = useParams();
+
     
-    useEffect(() => {
-        setUsername(info?.info?.login);
-    },);
+    // useEffect(() => {
+    //     setUsername(info?.info?.login);
+    // },);
 
     return (
         <div>
             <img src={avatarUrl+username} alt={username}></img>
+            <h1>{username}</h1>
         </div>
     )
 }
