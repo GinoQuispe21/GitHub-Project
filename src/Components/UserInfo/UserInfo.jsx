d7cd92d43cdb89e4bf44715e4da0d99bf90edf75
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-02-04 18:33:13 -0500

    Added react router push :zap:

--- src/Components/UserInfo/UserInfo.jsx
+++ src/Components/UserInfo/UserInfo.jsx
@@ -1,22 +1,31 @@
 import React, {useEffect} from 'react'
 import {useParams} from "react-router-dom";
+import axios from 'axios';
+import URL from '../../baseURL';
 
 const UserInfo = (info) => {
 
+    const [infoH, setInfo] = React.useState();
     //avatar_url
     const [avatarUrl, setAvatarUrl] = React.useState('https://avatars.githubusercontent.com/');
     // const [username, setUsername] = React.useState();
     let { username } = useParams();
 
     
-    // useEffect(() => {
-    //     setUsername(info?.info?.login);
-    // },);
+    useEffect(() => {
+        axios.get(`${URL}users/${username}`)
+        .then(res => {
+            setInfo(res.data);
+        });
+        console.log("aaaaaa");
+    },[]);
 
     return (
         <div>
             <img src={avatarUrl+username} alt={username}></img>
-            <h1>{username}</h1>
+            <h1><a href={infoH?.html_url} target="_BLANK" rel="noreferrer">{username}</a></h1>
+            <h3> Followers: {infoH?.followers}</h3>
+            <h3> Following: {infoH?.following}</h3>
         </div>
     )
 }
