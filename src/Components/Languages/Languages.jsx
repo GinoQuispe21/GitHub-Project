87641f7c659f197e9761c1120318f0e8b4b7773c
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-02-06 13:48:07 -0500

    ⚡️ Dictionary for languages
    Created dictionary/hashmap for language length values.

--- src/Components/Languages/Languages.jsx
+++ src/Components/Languages/Languages.jsx
@@ -7,19 +7,42 @@ import Ga from '../../Assets/ga.png'
 const Languages = () => {
 
     const [Repos, setInfo] = React.useState();
+    const [langs, setLangs] = React.useState();
     let { username } = useParams();
 
     useEffect(() => {
         axios.get(`${URL}users/${username}/repos`)
         .then(res => {
             setInfo(res.data);
+            return res;
+        }).then(res => {
+            let dic = {};
+            res.data.forEach(repo => {
+                axios.get(repo.languages_url).then(res =>{
+                    for(var key in res.data){
+                        if (res.data.hasOwnProperty(key)) {
+                            if(dic[key]){
+                                dic[key] += res.data[key];
+                            }
+                            else {
+                                dic[key] = res.data[key];
+                            }
+                        }
+                    }
+                });
+            });
+            console.log(dic);
+            for (var key in dic){
+                console.log( key, dic[key] );
+              }
+            
+            setLangs(dic);
         });
     },[username]);
 
     return (
         <div>
             <div>
-                <p>{JSON.stringify(Repos)}</p>
             </div>
             <img src={Ga}></img>
         </div>
